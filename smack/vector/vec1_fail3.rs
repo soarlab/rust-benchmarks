dff01231ed03f9637b975add3fbab6879a88782f
Zvonimir Rakamaric <zrakamaric@fb.com>
2020-02-07 15:04:27 -0700

    Updated vector benchmarks

--- smack/vector/vec1_fail3.rs
+++ smack/vector/vec1_fail3.rs
@@ -1,20 +1,16 @@
-#[macro_use]
-mod smack;
-use smack::*;
-
 // @flag --no-memory-splitting
 // @expect error
 
-fn main() {
-  let mut v: Vec<u64> = Vec::new();
-  v.push(0);
-  v.push(1);
-  v.push(3);
-  assert!(v[0] == 0);
-  assert!(v[1] == 1);
-  assert!(v[2] == 3);
-  v[2] = v[0]+v[1];
-  assert!(v[0] == 0);
-  assert!(v[1] == 1);
-  assert!(v[2] != 1);
+pub fn main() {
+    let mut v: Vec<u64> = Vec::new();
+    v.push(0);
+    v.push(1);
+    v.push(3);
+    verifier::assert!(v[0] == 0);
+    verifier::assert!(v[1] == 1);
+    verifier::assert!(v[2] == 3);
+    v[2] = v[0] + v[1];
+    verifier::assert!(v[0] == 0);
+    verifier::assert!(v[1] == 1);
+    verifier::assert!(v[2] != 1);
 }
