5298e3d701421372ade666cf1d567ae88e1a4415
Zvonimir Rakamaric <zrakamaric@fb.com>
2020-02-07 14:49:07 -0700

    Updated loops benchmarks

--- smack/loops/gauss_sum_nondet.rs
+++ smack/loops/gauss_sum_nondet.rs
@@ -1,16 +1,12 @@
-#[macro_use]
-mod smack;
-use smack::*;
-
 // @flag --no-memory-splitting --unroll=4
 // @expect verified
 
-fn main() {
-  let mut sum = 0;
-  let b = 7u64.nondet();
-  assume!(b < 5 && b > 1);
-  for i in 0..b as u64 {
-    sum += i;
-  }
-  assert!(2*sum == b*(b-1));
+pub fn main() {
+    let mut sum = 0;
+    let b = 7u64.nondet();
+    verifier::assume!(b < 5 && b > 1);
+    for i in 0..b as u64 {
+        sum += i;
+    }
+    verifier::assert!(2 * sum == b * (b - 1));
 }
