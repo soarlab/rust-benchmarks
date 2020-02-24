bb2952b511dd86ced2b93ba4cd21fbd1454d2be1
Zvonimir Rakamaric <zrakamaric@fb.com>
2020-02-23 23:35:55 -0700

    Fixed Fibonacci benchmark

--- benchmarks/smack-regressions/recursion/fib.rs
+++ benchmarks/smack-regressions/recursion/fib.rs
@@ -3,7 +3,7 @@
 
 fn fib(x: u64) -> u64 {
     match x {
-        0 => 1,
+        0 => 0,
         1 => 1,
         _ => fib(x - 1) + fib(x - 2),
     }
@@ -11,5 +11,5 @@ fn fib(x: u64) -> u64 {
 
 pub fn main() {
     let x = fib(6);
-    verifier::assert_eq!(x, 13);
+    verifier::assert_eq!(x, 8);
 }
